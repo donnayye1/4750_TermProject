e85ce786dd636f019c1a23cb21df2f235bba5db2
donnayye1 <103465796+donnayye1@users.noreply.github.com>
2022-04-18 20:53:32 -0500

    Update 4750_TP_ESP32_NodeMCS.ino

--- 4750_TP_ESP32_NodeMCS.ino
+++ 4750_TP_ESP32_NodeMCS.ino
@@ -1,7 +1,7 @@
 #define RX2 16
 #define TX2 17
 
-void setup() {
+void setup() {            //bing bong
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, RX2, TX2);
